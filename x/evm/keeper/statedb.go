6142d4a40a5b861b667cf1836ca1a092fabf73d0
George Miller <george.miller0042@gmail.com>
2023-12-20 22:34:36 +1100

    support cosmos base account and avoid the dedicate eth type account

--- x/evm/keeper/statedb.go
+++ x/evm/keeper/statedb.go
@@ -16,15 +16,15 @@
 package keeper
 
 import (
-	"math/big"
-
 	errorsmod "cosmossdk.io/errors"
 	"github.com/cosmos/cosmos-sdk/store/prefix"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
 	"github.com/ethereum/go-ethereum/common"
 	ethermint "github.com/evmos/ethermint/types"
 	"github.com/evmos/ethermint/x/evm/statedb"
 	"github.com/evmos/ethermint/x/evm/types"
+	"math/big"
 )
 
 var _ statedb.Keeper = &Keeper{}
@@ -117,9 +117,23 @@ func (k *Keeper) SetAccount(ctx sdk.Context, addr common.Address, account stated
 
 	codeHash := common.BytesToHash(account.CodeHash)
 
-	if ethAcct, ok := acct.(ethermint.EthAccountI); ok {
+	ethAcct, isEtherAcc := acct.(ethermint.EthAccountI)
+
+	if isEtherAcc {
+		//	if ethAcct, ok := acct.(ethermint.EthAccountI); ok {
 		if err := ethAcct.SetCodeHash(codeHash); err != nil {
 			return err
+		}
+	}
+
+	if !isEtherAcc && account.IsContract() {
+		if baseAcct, isBaseAccount := acct.(*authtypes.BaseAccount); isBaseAccount {
+			acct = &ethermint.EthAccount{
+				BaseAccount: baseAcct,
+				CodeHash:    codeHash.Hex(),
+			}
+		} else {
+			return errorsmod.Wrapf(types.ErrInvalidAccount, "type %T, address %s", acct, addr)
 		}
 	}
 
